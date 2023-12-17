6907a19c4f5be574384f478251ae448f6ac077d9
Inknyto <nytosene@gmail.com>
2023-12-17 06:28:08 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository is made specially for removing cookie banners from websites I visit the most.
+Maybe i will overcome my cookie-phobia one day
+Sorry stackoverflow
---
+++ for-stackoverflow-like-websites/content-script.js
@@ -0,0 +1,12 @@
+const gdpr = document.getElementsByClassName("js-consent-banner")[0]
+
+function removeGdprBanner() {
+  if (gdpr) {
+    gdpr.remove();
+  } else {
+    setTimeout(removeGdprBanner, 2000);
+  }
+}
+
+// Start the process
+removeGdprBanner();
